c8ffbb3f38d4af0b3d247ec4710ecf3a952ea2d0
Yukhimuk Dzmitry <yuhimuk.d@gmail.com>
2022-09-12 21:09:51 +0300

    icons to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import styles from "./Footer.module.scss"
-import {faInstagram, faLinkedin, faTelegram, faViber, faVk} from "@fortawesome/free-brands-svg-icons"
+import {faInstagram, faLinkedin, faTelegram, faViber, faVk, faGithub} from "@fortawesome/free-brands-svg-icons"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import CWIcon from "../../common/c3-img/codewars.svg"
 
 export const Footer = () => {
     return (
@@ -13,7 +14,10 @@ export const Footer = () => {
                     <a href={"viber://chat?number=%2B375295256082"}><FontAwesomeIcon icon={faViber} size={"2x"}/></a>
                     <a href={"https://vk.com/id12667881"}><FontAwesomeIcon icon={faVk} size={"2x"}/></a>
                     <a href={"https://www.instagram.com/yuhee2017/"}><FontAwesomeIcon icon={faInstagram} size={"2x"}/></a>
-                    <a href={"https://t.me/DzmitryYukhimuk"}><FontAwesomeIcon icon={faLinkedin} size={"2x"}/></a>
+                    <a href={"https://www.linkedin.com/in/dmitry-yukhimuk-65589324b/"}><FontAwesomeIcon icon={faLinkedin} size={"2x"}/></a>
+                    <a href={"https://www.codewars.com/users/Yuhee2020"}><img src={CWIcon}/></a>
+                    <a href={"https://www.linkedin.com/in/dmitry-yukhimuk-65589324b/"}><FontAwesomeIcon icon={faGithub} size={"2x"}/></a>
+
                 </div>
             </div>
         </div>
